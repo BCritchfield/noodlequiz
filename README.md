e2059584873f375dd07f6f0c2a3b8289d057a744
Brittany <brittany.critchfield.20@dartmouth.edu>
2020-04-16 01:49:07 -0400

    it works kinda!

---
+++ README.md
@@ -0,0 +1,6 @@
+Lab 2
+Brittany Critchfield
+
+For this lab I implemented a modal for the results which fades in. I chose to use JQuery. 
+
+Scoring: the result is calculated on a weighted scale. Each answer has a certain weight associated it, your score at the end determines your result. 
--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 $('#submit-button').on('click', function(e) {
     // gather all checked radio-button values
     var choices = $("input[type='radio']:checked").map(function(i, radio) {
-      return $(radio).val();
+      return parseInt($(radio).val(), 10);
     }).toArray();
-    console.log('choices');
-    console.log(choices);
+
+    let result = choices.reduce(function(a, b){
+      return a + b;
+    }, 0);
+
+    if (result > 5){
+      $('#result3').addClass('display');
+      $('#result2').addClass('hide');
+      $('#result1').addClass('hide');
+    }
+
+    else if (result > 3){
+      $('#result2').addClass('display');
+      $('#result1').addClass('hide');
+      $('#result3').addClass('hide');
+    }
+
+    else{
+      $('#result1').addClass('display');
+      $('#result2').addClass('hide');
+      $('#result3').addClass('hide');
+
+    }
+
     
     $('.result').fadeIn();
     // now you have an array of choices = ["valueofradiobox1", "valueofradiobox2", "valueofradiobox2"]
